c04fcd2fb2cc68b494e88e9d118904c3e9bba9d0
KhoaNAD1 <khoanad@live.com>
2022-05-28 16:56:54 +0700

    init project

---
+++ testcases/com/nopecommerce/user/User_01_Register_Login.java
@@ -0,0 +1,34 @@
+package com.nopecommerce.user;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class User_01_Register_Login {
+
+	@BeforeClass
+	public void beforeClass() {
+		
+	}
+	
+	@Test
+	public void TC_01_Register_Empty_Dat() {
+		
+	}
+	
+	@Test
+	public void TC_02_Register_Invalid_Email() {
+		
+	}
+	
+	@Test
+	public void TC_03_Register_Invalid_Password() {
+		
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		
+	}
+	
+}
---
+++ testdata/com/nopcomerce/data/User.java
@@ -0,0 +1,18 @@
+package com.nopcomerce.data;
+
+public class User {
+
+	public class Regiser{
+		
+	}
+	
+	public class Login{
+		
+	}
+	
+	public class Order{
+		
+		
+	}
+	
+}
